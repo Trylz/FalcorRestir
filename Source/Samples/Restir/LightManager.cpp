3ebe46b2056e5c9a12d798e8d09d376925db284b
Cloyz2021 <84689194+Cloyz2021@users.noreply.github.com>
2025-02-27 22:44:08 -0500

    Update LightManager.cpp

--- Source/Samples/Restir/LightManager.cpp
+++ Source/Samples/Restir/LightManager.cpp
@@ -106,7 +106,7 @@ void LightManager::spawnRandomSanMiguelLights(
     FloatRandomNumberGenerator& rng
 )
 {
-    const uint32_t nbightsPerAxis = 4;
+    const uint32_t nbightsPerAxis = 8;
 
     for (auto& ext : extents)
     {
@@ -121,7 +121,7 @@ void LightManager::spawnRandomSanMiguelLights(
 
             light.mColor =
                 Falcor::float3(rng.generateUnsignedNormalized(), rng.generateUnsignedNormalized(), rng.generateUnsignedNormalized()) *
-                4000.0f;
+                2000.0f;
             light.mWsPosition = controlPoint + (float)i * delta;
 
             mLights.push_back(light);
