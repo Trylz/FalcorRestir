502f3157b82ce104f5d726f37d63c8799d730c88
Cloyz2021 <84689194+Cloyz2021@users.noreply.github.com>
2025-02-28 20:34:45 -0500

    Update DenoisingPass.cpp

--- Source/Samples/Restir/DenoisingPass.cpp
+++ Source/Samples/Restir/DenoisingPass.cpp
@@ -1,1 +1,14 @@
 #include "DenoisingPass.h"
+
+#if defined(_DEBUG)
+    #pragma comment(lib, __FILE__ "\\Dependencies\\NvidiaNRD\\lib\\Debug\\NRD.lib")
+#else
+    #pragma comment(lib, __FILE__ "\\..\\Dependencies\\NvidiaNRD\\lib\\Release\\NRD.lib")
+#endif
+
+
+#if defined(_DEBUG)
+    #pragma comment(lib, __FILE__ "\\Dependencies\\NvidiaNRD\\lib\\Debug\\NRD.lib")
+#else
+    #pragma comment(lib, __FILE__ "\\..\\Dependencies\\NvidiaNRD\\lib\\Release\\NRD.lib")
+#endif
