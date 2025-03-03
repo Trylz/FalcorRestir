af23970d92fcc125dc5aef348a9006585c615f2e
Cloyz2021 <84689194+Cloyz2021@users.noreply.github.com>
2025-03-02 20:25:00 -0500

    Update DenoisingPass.cpp

--- Source/Samples/Restir/DenoisingPass.cpp
+++ Source/Samples/Restir/DenoisingPass.cpp
@@ -35,14 +35,129 @@ DenoisingPass::DenoisingPass(
 {
     mpDevice->requireD3D12();
 
-    DefineList definesRelax;
-    definesRelax.add("NRD_USE_OCT_NORMAL_ENCODING", "1");
-    definesRelax.add("NRD_USE_MATERIAL_ID", "0");
-    definesRelax.add("NRD_METHOD", "0"); // NRD_METHOD_RELAX_DIFFUSE_SPECULAR
-    mpPackNRDPass = ComputePass::create(pDevice, "Samples/Restir/DenoisingPass_PackNRD.slang", "PackNRD", definesRelax);
-    mpUnpackNRDPass = ComputePass::create(pDevice, "Samples/Restir/DenoisingPass_UnpackNRD.slang", "UnpackNRD", definesRelax);
+    mpPackNRDPass = ComputePass::create(pDevice, "Samples/Restir/DenoisingPass_PackNRD.slang", "PackNRD");
+    mpUnpackNRDPass = ComputePass::create(pDevice, "Samples/Restir/DenoisingPass_UnpackNRD.slang", "UnpackNRD");
 
     createFalcorTextures(pDevice);
+    initNRD();
+}
+
+static void* nrdAllocate(void* userArg, size_t size, size_t alignment)
+{
+    return malloc(size);
+}
+
+static void* nrdReallocate(void* userArg, void* memory, size_t size, size_t alignment)
+{
+    return realloc(memory, size);
+}
+
+static void nrdFree(void* userArg, void* memory)
+{
+    free(memory);
+}
+
+static ResourceFormat getFalcorFormat(nrd::Format format)
+{
+    switch (format)
+    {
+    case nrd::Format::R8_UNORM:
+        return ResourceFormat::R8Unorm;
+    case nrd::Format::R8_SNORM:
+        return ResourceFormat::R8Snorm;
+    case nrd::Format::R8_UINT:
+        return ResourceFormat::R8Uint;
+    case nrd::Format::R8_SINT:
+        return ResourceFormat::R8Int;
+    case nrd::Format::RG8_UNORM:
+        return ResourceFormat::RG8Unorm;
+    case nrd::Format::RG8_SNORM:
+        return ResourceFormat::RG8Snorm;
+    case nrd::Format::RG8_UINT:
+        return ResourceFormat::RG8Uint;
+    case nrd::Format::RG8_SINT:
+        return ResourceFormat::RG8Int;
+    case nrd::Format::RGBA8_UNORM:
+        return ResourceFormat::RGBA8Unorm;
+    case nrd::Format::RGBA8_SNORM:
+        return ResourceFormat::RGBA8Snorm;
+    case nrd::Format::RGBA8_UINT:
+        return ResourceFormat::RGBA8Uint;
+    case nrd::Format::RGBA8_SINT:
+        return ResourceFormat::RGBA8Int;
+    case nrd::Format::RGBA8_SRGB:
+        return ResourceFormat::RGBA8UnormSrgb;
+    case nrd::Format::R16_UNORM:
+        return ResourceFormat::R16Unorm;
+    case nrd::Format::R16_SNORM:
+        return ResourceFormat::R16Snorm;
+    case nrd::Format::R16_UINT:
+        return ResourceFormat::R16Uint;
+    case nrd::Format::R16_SINT:
+        return ResourceFormat::R16Int;
+    case nrd::Format::R16_SFLOAT:
+        return ResourceFormat::R16Float;
+    case nrd::Format::RG16_UNORM:
+        return ResourceFormat::RG16Unorm;
+    case nrd::Format::RG16_SNORM:
+        return ResourceFormat::RG16Snorm;
+    case nrd::Format::RG16_UINT:
+        return ResourceFormat::RG16Uint;
+    case nrd::Format::RG16_SINT:
+        return ResourceFormat::RG16Int;
+    case nrd::Format::RG16_SFLOAT:
+        return ResourceFormat::RG16Float;
+    case nrd::Format::RGBA16_UNORM:
+        return ResourceFormat::RGBA16Unorm;
+    case nrd::Format::RGBA16_SNORM:
+        return ResourceFormat::Unknown; // Not defined in Falcor
+    case nrd::Format::RGBA16_UINT:
+        return ResourceFormat::RGBA16Uint;
+    case nrd::Format::RGBA16_SINT:
+        return ResourceFormat::RGBA16Int;
+    case nrd::Format::RGBA16_SFLOAT:
+        return ResourceFormat::RGBA16Float;
+    case nrd::Format::R32_UINT:
+        return ResourceFormat::R32Uint;
+    case nrd::Format::R32_SINT:
+        return ResourceFormat::R32Int;
+    case nrd::Format::R32_SFLOAT:
+        return ResourceFormat::R32Float;
+    case nrd::Format::RG32_UINT:
+        return ResourceFormat::RG32Uint;
+    case nrd::Format::RG32_SINT:
+        return ResourceFormat::RG32Int;
+    case nrd::Format::RG32_SFLOAT:
+        return ResourceFormat::RG32Float;
+    case nrd::Format::RGB32_UINT:
+        return ResourceFormat::RGB32Uint;
+    case nrd::Format::RGB32_SINT:
+        return ResourceFormat::RGB32Int;
+    case nrd::Format::RGB32_SFLOAT:
+        return ResourceFormat::RGB32Float;
+    case nrd::Format::RGBA32_UINT:
+        return ResourceFormat::RGBA32Uint;
+    case nrd::Format::RGBA32_SINT:
+        return ResourceFormat::RGBA32Int;
+    case nrd::Format::RGBA32_SFLOAT:
+        return ResourceFormat::RGBA32Float;
+    case nrd::Format::R10_G10_B10_A2_UNORM:
+        return ResourceFormat::RGB10A2Unorm;
+    case nrd::Format::R10_G10_B10_A2_UINT:
+        return ResourceFormat::RGB10A2Uint;
+    case nrd::Format::R11_G11_B10_UFLOAT:
+        return ResourceFormat::R11G11B10Float;
+    case nrd::Format::R9_G9_B9_E5_UFLOAT:
+        return ResourceFormat::RGB9E5Float;
+    default:
+        FALCOR_THROW("Unsupported NRD format.");
+    }
+}
+
+static void copyMatrix(float* dstMatrix, const float4x4& srcMatrix)
+{
+    float4x4 col_major = transpose(srcMatrix);
+    memcpy(dstMatrix, static_cast<const float*>(col_major.data()), sizeof(float4x4));
 }
 
 void DenoisingPass::createFalcorTextures(Falcor::ref<Falcor::Device> pDevice)
@@ -84,21 +199,6 @@ DenoisingPass::~DenoisingPass()
 }
 
 
-static void* nrdAllocate(void* userArg, size_t size, size_t alignment)
-{
-    return malloc(size);
-}
-
-static void* nrdReallocate(void* userArg, void* memory, size_t size, size_t alignment)
-{
-    return realloc(memory, size);
-}
-
-static void nrdFree(void* userArg, void* memory)
-{
-    free(memory);
-}
-
 void DenoisingPass::initNRD()
 {
     mpDenoiser = nullptr;
@@ -121,6 +221,166 @@ void DenoisingPass::initNRD()
 
     createResources();
     createPipelines();
+}
+
+void DenoisingPass::createPipelines()
+{
+    mpPasses.clear();
+    mpCachedProgramKernels.clear();
+    mpCSOs.clear();
+    mCBVSRVUAVdescriptorSetLayouts.clear();
+    mpRootSignatures.clear();
+
+    // Get denoiser desc for currently initialized denoiser implementation.
+    const nrd::DenoiserDesc& denoiserDesc = nrd::GetDenoiserDesc(*mpDenoiser);
+
+    // Create samplers descriptor layout and set.
+    D3D12DescriptorSetLayout SamplersDescriptorSetLayout;
+
+    for (uint32_t j = 0; j < denoiserDesc.staticSamplerNum; j++)
+    {
+        SamplersDescriptorSetLayout.addRange(ShaderResourceType::Sampler, denoiserDesc.staticSamplers[j].registerIndex, 1);
+    }
+    mpSamplersDescriptorSet =
+        D3D12DescriptorSet::create(mpDevice, SamplersDescriptorSetLayout, D3D12DescriptorSetBindingUsage::ExplicitBind);
+
+    // Set sampler descriptors right away.
+    for (uint32_t j = 0; j < denoiserDesc.staticSamplerNum; j++)
+    {
+        mpSamplersDescriptorSet->setSampler(0, j, mpSamplers[j].get());
+    }
+
+    // Go over NRD passes and creating descriptor sets, root signatures and PSOs for each.
+    for (uint32_t i = 0; i < denoiserDesc.pipelineNum; i++)
+    {
+        const nrd::PipelineDesc& nrdPipelineDesc = denoiserDesc.pipelines[i];
+        const nrd::ComputeShader& nrdComputeShader = nrdPipelineDesc.computeShaderDXIL;
+
+        // Initialize descriptor set.
+        D3D12DescriptorSetLayout CBVSRVUAVdescriptorSetLayout;
+
+        // Add constant buffer to descriptor set.
+        CBVSRVUAVdescriptorSetLayout.addRange(ShaderResourceType::Cbv, denoiserDesc.constantBufferDesc.registerIndex, 1);
+
+        for (uint32_t j = 0; j < nrdPipelineDesc.descriptorRangeNum; j++)
+        {
+            const nrd::DescriptorRangeDesc& nrdDescriptorRange = nrdPipelineDesc.descriptorRanges[j];
+
+            ShaderResourceType descriptorType = nrdDescriptorRange.descriptorType == nrd::DescriptorType::TEXTURE
+                                                    ? ShaderResourceType::TextureSrv
+                                                    : ShaderResourceType::TextureUav;
+
+            CBVSRVUAVdescriptorSetLayout.addRange(descriptorType, nrdDescriptorRange.baseRegisterIndex, nrdDescriptorRange.descriptorNum);
+        }
+
+        mCBVSRVUAVdescriptorSetLayouts.push_back(CBVSRVUAVdescriptorSetLayout);
+
+        // Create root signature for the NRD pass.
+        D3D12RootSignature::Desc rootSignatureDesc;
+        rootSignatureDesc.addDescriptorSet(SamplersDescriptorSetLayout);
+        rootSignatureDesc.addDescriptorSet(CBVSRVUAVdescriptorSetLayout);
+
+        const D3D12RootSignature::Desc& desc = rootSignatureDesc;
+
+        ref<D3D12RootSignature> pRootSig = D3D12RootSignature::create(mpDevice, desc);
+
+        mpRootSignatures.push_back(pRootSig);
+
+        // Create Compute PSO for the NRD pass.
+        {
+            std::string shaderFileName = "nrd/Shaders/Source/" + std::string(nrdPipelineDesc.shaderFileName) + ".hlsl";
+
+            ProgramDesc programDesc;
+            programDesc.addShaderLibrary(shaderFileName).csEntry(nrdPipelineDesc.shaderEntryPointName);
+            programDesc.setCompilerFlags(SlangCompilerFlags::MatrixLayoutColumnMajor);
+            // Disable warning 30056: non-short-circuiting `?:` operator is deprecated, use 'select' instead.
+            programDesc.setCompilerArguments({"-Wno-30056"});
+            DefineList defines;
+            defines.add("NRD_COMPILER_DXC");
+            defines.add("NRD_USE_OCT_NORMAL_ENCODING", "1");
+            defines.add("NRD_USE_MATERIAL_ID", "0");
+            ref<ComputePass> pPass = ComputePass::create(mpDevice, programDesc, defines);
+
+            ref<Program> pProgram = pPass->getProgram();
+            ref<const ProgramKernels> pProgramKernels = pProgram->getActiveVersion()->getKernels(mpDevice.get(), pPass->getVars().get());
+
+            ComputeStateObjectDesc csoDesc;
+            csoDesc.pProgramKernels = pProgramKernels;
+            csoDesc.pD3D12RootSignatureOverride = pRootSig;
+
+            ref<ComputeStateObject> pCSO = mpDevice->createComputeStateObject(csoDesc);
+
+            mpPasses.push_back(pPass);
+            mpCachedProgramKernels.push_back(pProgramKernels);
+            mpCSOs.push_back(pCSO);
+        }
+    }
+}
+
+void DenoisingPass::createResources()
+{
+    // Destroy previously created resources.
+    mpSamplers.clear();
+    mpPermanentTextures.clear();
+    mpTransientTextures.clear();
+
+    const nrd::DenoiserDesc& denoiserDesc = nrd::GetDenoiserDesc(*mpDenoiser);
+    const uint32_t poolSize = denoiserDesc.permanentPoolSize + denoiserDesc.transientPoolSize;
+
+    // Create samplers.
+    for (uint32_t i = 0; i < denoiserDesc.staticSamplerNum; i++)
+    {
+        const nrd::StaticSamplerDesc& nrdStaticsampler = denoiserDesc.staticSamplers[i];
+        Sampler::Desc samplerDesc;
+        samplerDesc.setFilterMode(TextureFilteringMode::Linear, TextureFilteringMode::Linear, TextureFilteringMode::Point);
+
+        if (nrdStaticsampler.sampler == nrd::Sampler::NEAREST_CLAMP || nrdStaticsampler.sampler == nrd::Sampler::LINEAR_CLAMP)
+        {
+            samplerDesc.setAddressingMode(TextureAddressingMode::Clamp, TextureAddressingMode::Clamp, TextureAddressingMode::Clamp);
+        }
+        else
+        {
+            samplerDesc.setAddressingMode(TextureAddressingMode::Mirror, TextureAddressingMode::Mirror, TextureAddressingMode::Mirror);
+        }
+
+        if (nrdStaticsampler.sampler == nrd::Sampler::NEAREST_CLAMP || nrdStaticsampler.sampler == nrd::Sampler::NEAREST_MIRRORED_REPEAT)
+        {
+            samplerDesc.setFilterMode(TextureFilteringMode::Point, TextureFilteringMode::Point, TextureFilteringMode::Point);
+        }
+        else
+        {
+            samplerDesc.setFilterMode(TextureFilteringMode::Linear, TextureFilteringMode::Linear, TextureFilteringMode::Point);
+        }
+
+        mpSamplers.push_back(mpDevice->createSampler(samplerDesc));
+    }
+
+    // Texture pool.
+    for (uint32_t i = 0; i < poolSize; i++)
+    {
+        const bool isPermanent = (i < denoiserDesc.permanentPoolSize);
+
+        // Get texture desc.
+        const nrd::TextureDesc& nrdTextureDesc =
+            isPermanent ? denoiserDesc.permanentPool[i] : denoiserDesc.transientPool[i - denoiserDesc.permanentPoolSize];
+
+        // Create texture.
+        ResourceFormat textureFormat = getFalcorFormat(nrdTextureDesc.format);
+        ref<Texture> pTexture = mpDevice->createTexture2D(
+            nrdTextureDesc.width,
+            nrdTextureDesc.height,
+            textureFormat,
+            1u,
+            nrdTextureDesc.mipNum,
+            nullptr,
+            ResourceBindFlags::ShaderResource | ResourceBindFlags::UnorderedAccess
+        );
+
+        if (isPermanent)
+            mpPermanentTextures.push_back(pTexture);
+        else
+            mpTransientTextures.push_back(pTexture);
+    }
 }
 
 void DenoisingPass::packNRD(Falcor::RenderContext* pRenderContext)
